ab8244ddcb7ab1ef7aeae2580eb3b68c73ea2176
RyuKH95 <gh9503@gmail.com>
2022-01-24 00:09:26 +0900

    home

---
+++ src/main/java/rkh/webProject/controller/HomeContoller.java
@@ -0,0 +1,17 @@
+package rkh.webProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeContoller {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/colorvely")
+    public String colorvely() {
+        return "colorvely";
+    }
+}
