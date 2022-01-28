07e0e6be43b2fa4bb916ca1796f8e97d9a45aadf
RyuKH95 <gh9503@gmail.com>
2022-01-28 14:44:24 +0900

    회원가입form

--- src/main/java/rkh/webProject/controller/CompanyController.java
+++ src/main/java/rkh/webProject/controller/CompanyController.java
@@ -2,6 +2,9 @@ package rkh.webProject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import rkh.webProject.domain.Company;
 import rkh.webProject.service.CompanyService;
 
 @Controller
@@ -11,5 +14,18 @@ public class CompanyController {
     @Autowired
     public CompanyController(CompanyService companyService) {
         this.companyService = companyService;
+    }
+
+    @GetMapping("/company/new")
+    public String createForm() {
+        return "company/createCompanyForm";
+    }
+    @PostMapping("/company/new")
+    public String companyCreate(CompanyForm form) {
+        Company company = new Company();
+        company.setName(form.getName());
+
+        companyService.join(company);
+        return "redirect:/spring";
     }
 }
